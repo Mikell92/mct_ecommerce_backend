61e9b17064be0b98b97b64b85c75a7a36ab29335
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-07-23 10:57:57 -0600

    Initial commit: Spring Boot backend project

---
+++ src/test/java/com/muebleria/mctecommercebackend/MctEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muebleria.mctecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MctEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
